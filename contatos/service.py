54f0a1cb62fd954222a4d46073007fb7da671fbb
NICOLAS ALEXANDRE MACHADO <nicolas.machado@alunos.sc.senac.br>
2021-09-21 20:30:06 -0300

    Pesquisa

--- contatos/service.py
+++ contatos/service.py
@@ -1,7 +1,18 @@
 from.models import Contato
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 def buscar_todos_contatos():
-    return Contato.objects.all()
+    return Contato.objects.all().filter(
+        ativo=True
+    )
 
 def buscar_contatos_por_id(id:int) -> Contato:
     return Contato.objects.filter(id=id).first()
+
+def buscar_contatos_por_termo(termo:str):
+    campos = Concat('nome', Value(' '),'sobre_nome')
+    return Contato.objects.annotate(nome_completo=campos).filter(
+    Q(nome_completo__icontains=termo) | Q(sobre_nome__icontains=termo),
+        ativo=True,
+    )
--- contatos/urls.py
+++ contatos/urls.py
@@ -1,8 +1,9 @@
 from django.urls.conf import include
-from .views import ContatosView
+from .views import ContatosView, ContatosBuscaView
 from django.urls import path
 
 urlpatterns = [
     path('', ContatosView.as_view(), name='home'),
-    path('<int:contato_id>', ContatosView.as_view(), name='ver_contato')
+    path('<int:contato_id>', ContatosView.as_view(), name='ver_contato'),
+    path('busca/', ContatosBuscaView.as_view(), name='busca')
 ]
